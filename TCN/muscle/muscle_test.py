5693cdd0826383e6f57e9e06e71748a2b4b7232d
xingce.bao <xingce.bao@gmail.com>
2021-03-19 00:50:02 +0800

    start test func

---
+++ TCN/muscle/muscle_test.py
@@ -0,0 +1,66 @@
+import argparse
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+import sys
+
+from TCN.muscle.model import TCN
+
+sys.path.append("../../")
+from TCN.muscle.utils import data_generator
+import numpy as np
+
+
+parser = argparse.ArgumentParser(description="Jirou Feng's data")
+parser.add_argument('--cuda', action='store_false',
+                    help='use CUDA (default: True)')
+parser.add_argument('--dropout', type=float, default=0.25,
+                    help='dropout applied to layers (default: 0.25)')
+parser.add_argument('--clip', type=float, default=0.2,
+                    help='gradient clip, -1 means no clip (default: 0.2)')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='upper epoch limit (default: 100)')
+parser.add_argument('--ksize', type=int, default=5,
+                    help='kernel size (default: 5)')
+parser.add_argument('--levels', type=int, default=4,
+                    help='# of levels (default: 4)')
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                    help='report interval (default: 100')
+parser.add_argument('--lr', type=float, default=1e-3,
+                    help='initial learning rate (default: 1e-3)')
+parser.add_argument('--optim', type=str, default='Adam',
+                    help='optimizer to use (default: Adam)')
+parser.add_argument('--nhid', type=int, default=150,
+                    help='number of hidden units per layer (default: 150)')
+parser.add_argument('--input_size', type=int, default=88,
+                    help='input size (default: 88)')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed (default: 1111)')
+
+args = parser.parse_args()
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+print(args)
+
+training_X, training_Y, validation_X, validation_Y, test_X, test_Y = data_generator(args.data)
+
+input_size = args.input_size
+n_channels = [args.nhid] * args.levels
+kernel_size = args.ksize
+dropout = args.dropout
+
+model = TCN(input_size, input_size, n_channels, kernel_size, dropout=args.dropout)
+
+if args.cuda:
+    model.cuda()
+
+criterion = nn.NLLLoss()
+lr = args.lr
+optimizer = getattr(optim, args.optim)(model.parameters(), lr=lr)
+
